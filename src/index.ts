ec5ef385a0a9b6f0b021d67757c81dbf3af68d11
ramanan1998 <ramananr2698095@gmail.com>
2025-03-27 22:36:40 +0530

    nodejs typescript boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
-const app = express();
 const PORT = 5000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello TypeScript with Express!");
